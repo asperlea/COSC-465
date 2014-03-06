6e47e4ff8673cd8aa12350200c803267c059fc32
Curt Mahoney <cwmahoney@colgate.edu>
2014-03-06 16:10:19 -0500

    Done with all the tests, now just ot run on MN

--- Project 4/myrouter2.py
+++ Project 4/myrouter2.py
@@ -17,172 +17,228 @@ from pox.lib.packet import ethernet,ETHER_BROADCAST,IP_ANY
 from pox.lib.packet import arp
 from pox.lib.addresses import EthAddr,IPAddr,netmask_to_cidr
 from srpy_common import log_info, log_debug, log_warn, SrpyShutdown, SrpyNoPackets, debugger
+import time
 
 class Router(object):
-    def __init__(self, net):
-	self.net = net
-	self.ip_to_ether = {} #Empty dict to store mappings
-	self.my_interfaces = {}
+	def __init__(self, net):
+		self.net = net
 		
-	for intf in net.interfaces():
-	    self.my_interfaces[intf.ipaddr] = intf.ethaddr
+		self.ip_to_ether = {} #Empty dict to store mappings
+		self.my_interfaces = Set() #Set of ip's for this router's interfaces
+		self.forwardingTable = {}
+		self.nameMap = {} #Maps from intf names to ip and eth addresses
+		
+		self.buildMappings()		
+	
+	def makeReply(self, dst_ip, src_ip, hwdst, src_eth):
+		'''
+		Creates an ARP reply packet given IP source and destination, Ethernet source and destination
+		'''
+
+		arp_rep = arp() #Build up packet
+		
+		arp_rep.opcode = arp.REPLY
+		arp_rep.protosrc = dst_ip
+		arp_rep.protodst = src_ip
+		arp_rep.hwsrc = hwdst #Matching ethernet asked for in initial request
+		arp_rep.hwdst = src_eth
 			
-    def findEth(self, ip, ipToEther):
-	'''
-        Returns the Ethernet address for an IP address given a dictionary of IP to ethernet
-        '''
-        if ip in ipToEther:
-            return ipToEther[ip]
-        return None
+		ether = ethernet() #wrap in ether
+		ether.type = ether.ARP_TYPE
+		ether.set_payload(arp_rep)
+		ether.src = hwdst
+		ether.dst = src_eth
 
-    def addToEth(self, ip, eth, ipToEther):
-        '''
-        Adds an IP to Ethernet address to a dictionary of IP to ethernet
-        '''
-        if not (ip in ipToEther):
-            ipToEther[ip] = eth
+		return ether
 
-    def makeReply(self, dst_ip, src_ip, hwdst, src_eth):
-        '''
-        Creates an ARP reply packet given IP source and destination, Ethernet source and destination
-        '''
+	def makeRequest(self, dst_ip, src_ip, src_eth):
+		'''
+		Creates an ARP request packet
+		'''
 
-        arp_rep = arp() #Build up packet
+		arp_req = arp()
+
+		arp_req.opcode = arp.REQUEST
+		arp_req.protosrc = src_ip
+		arp_req.protodst = dst_ip
+		arp_req.hwsrc = src_eth
+		arp_req.hwdst = ETHER_BROADCAST
+
+		ether = ethernet() #wrap in ether
+		ether.type = ether.ARP_TYPE
+		ether.set_payload(arp_req)
+		ether.src = src_eth
+		ether.dst = ETHER_BROADCAST
 		
-	arp_rep.opcode = arp.REPLY
-	arp_rep.protosrc = dst_ip
-	arp_rep.protodst = src_ip
-	arp_rep.hwsrc = hwdst #Matching ether
-	arp_rep.hwdst = src_eth
+		return ether
 		
-	ether = ethernet() #wrap in ether
-	ether.type = ether.ARP_TYPE
-	ether.set_payload(arp_rep)
-	ether.src = hwdst
-	ether.dst = src_eth
+		
+	def buildMappings(self):
+		'''
+		Creates a Forwarding Table for the router, as well as establishing mappings from
+		names to eth and ip, storing a list of ip's associated with my interfaces, and
+		initializing the table of mappings from ip to eth addresses
+		
+		All addresses in forwarding table are stored in binary for easy bitwise operations
+		'''
+		
+		#Obtain routes from net.interfaces
+		for intf in self.net.interfaces():
+			prefix = intf.ipaddr.toUnsigned() & intf.netmask.toUnsigned() # network prefix
+			mask = intf.netmask # network mask
+			nexthop = None # next hop
+			name = intf.name # interface name
 
-        return ether
+			self.forwardingTable[prefix] = tuple([mask, nexthop, name])
+			
+			self.nameMap[name] = (intf.ethaddr, intf.ipaddr)
+			self.my_interfaces.add(intf.ipaddr)
+			self.ip_to_ether[intf.ipaddr] = intf.ethaddr
 
-    def makeRequest(self, dst_ip, src_ip, src_eth, dest):
-        '''
-        Creates an ARP request packet
-        '''
-
-        arp_req = arp()
-
-        arp_req.opcode = arp.REQUEST
-        arp_req.protosrc = src_ip
-        arp_req.protodst = dst_ip
-        arp_req.hwsrc = src_eth
-
-       	ether = ethernet() #wrap in ether
-	ether.type = ether.ARP_TYPE
-	ether.set_payload(arp_req)
-	ether.src = src_eth
-        ether.dst = 
-
-        return ether
-        
-        
-    def createForwardingTable(self):
-        '''
-        Creates a Forwarding Table for the router.
-        All addresses in forwarding table are stored in binary for easy bitwise operations
-        '''
-        self.forwardingTable = {}
-        self.myIP = Set()
-        
-        #Obtain routes from net.interfaces
-        for intf in self.net.interfaces():
-            prefix = intf.ipaddr.toUnsigned() & intf.netmask.toUnsigned() # network prefix
-            mask = intf.netmask # network mask
-            nexthop = None # next hop
-            name = intf.name # interface name
-
-            self.forwardingTable[prefix] = tuple([mask, nexthop, name])
-            self.myIP.add(intf.ipaddr)
-
-        #Obtain routes from forwarding_table.txt
-        forTable = open("forwarding_table.txt", "r")
-        for line in forTable:
-            parsedLine = line.split(" ")
-            prefix = IPAddr(parsedLine[0]).toUnsigned() # network prefix
-            mask = IPAddr(parsedLine[1]) # network mask
-            nexthop = IPAddr(parsedLine[2]) # next hop
-            name = parsedLine[3][0:-1] # interface name
-                       
-            self.forwardingTable[prefix] = tuple([mask, nexthop, name])
-       
-    def matchPrefix(self, dstip):
-        best_prefix = None
-        best_match = -1
-        for prefix in self.forwardingTable:
-            netmask = self.forwardingTable[prefix][0] # 0 is the netmask
-            if prefix == (dstip.toUnsigned() & netmask.toUnsigned()):
-                if best_match < netmask_to_cidr(netmask):
-                    best_prefix = prefix
-                    best_match = netmask_to_cidr(netmask)
-
-        return self.forwardingTable[best_prefix] # 1 is the nexthop
-        
-    def router_main(self):
-        self.createForwardingTable()
-
-	while True:
-	    try:
-		dev,ts,pkt = self.net.recv_packet(timeout=1.0)
-		payload = pkt.payload
+		#Obtain routes from forwarding_table.txt
+		forTable = open("forwarding_table.txt", "r")
+		for line in forTable:
+			parsedLine = line.split(" ")
+			prefix = IPAddr(parsedLine[0]).toUnsigned() # network prefix
+			mask = IPAddr(parsedLine[1]) # network mask
+			nexthop = IPAddr(parsedLine[2]) # next hop
+			name = parsedLine[3][0:-1] # interface name
+					   
+			self.forwardingTable[prefix] = tuple([mask, nexthop, name])
+	   
+	def matchPrefix(self, dstip):
+		best_prefix = None
+		best_match = -1
+		for prefix in self.forwardingTable:
+			netmask = self.forwardingTable[prefix][0] # 0 is the netmask
+			if prefix == (dstip.toUnsigned() & netmask.toUnsigned()):
+				if best_match ==-1 or best_match < netmask_to_cidr(netmask):
+					best_prefix = prefix
+					best_match = netmask_to_cidr(netmask)
+					
+		if best_prefix == None:
+			return None
+		return self.forwardingTable[best_prefix] #Index 1 is the nexthop
+		
+	def examineStalled(self, arp_ip):
+		keys = arp_ip.keys() #Can/will lose keys during iteration
+		for dst in keys:
+			stalled = arp_ip.pop(dst)
+			
+			if dst in self.ip_to_ether: #We've since figured this one out
+				dst_eth = self.ip_to_ether[dst]
 				
-		if pkt.type == pkt.ARP_TYPE: #Is an ARP
-	            src_ip = payload.protosrc
-        	    dst_ip = payload.protodst
-		    src_eth = payload.hwsrc
-		    #no dst
+				for ether_pkt in stalled.getList():
+					ether_pkt.dst = dst_eth
+					self.net.send_packet(stalled.intf_name, ether_pkt)
+			else:
+				difference = time.time() - stalled.start_time
+			
+				if difference/stalled.tries > 1:
+					if stalled.tries >=5: #Timeout, call it a day on this packet
+						continue
+					else:
+						self.net.send_packet(stalled.intf_name, stalled.arp_req) #Send ARP again
+						stalled.tries += 1
 					
-                    self.addToEth(src_ip, src_eth, self.ip_to_ether) #For later use\
+				arp_ip[dst] = stalled
+				
+	def router_main(self):
+		arp_ip = {} #Empty dict for IP's we're waiting for ARPs on
+		#Dict because it's faster than queue and we don't care about order
+		
+		while True:
+			try:
+				self.examineStalled(arp_ip) #deal with stalled waiting on ARPs
+				
+				dev,ts,pkt = self.net.recv_packet(timeout=1.0)
+				payload = pkt.payload
+				
+				if pkt.type == pkt.ARP_TYPE: #Is an ARP
+					src_ip = payload.protosrc
+					dst_ip = payload.protodst
+					src_eth = payload.hwsrc
+					#no dst
 					
-                    hwdst = self.findEth(dst_ip) #look up eth
+					self.ip_to_ether[src_ip] = src_eth #Add to map from IP's to eth
+					
+					if payload.opcode == arp.REQUEST:					
+						if dst_ip in self.ip_to_ether:
+							hwdst = self.ip_to_ether[dst_ip]
+							reply = self.makeReply(dst_ip, src_ip, hwdst, src_eth)
+							self.net.send_packet(dev, reply) #send it off
+						#Do nothing otherwise
+					else: #ARP_REPLY
+						self.ip_to_ether[src_ip] = src_eth
+						#Sending dealt with in examineStalled(.) 
+						
+				
+				elif pkt.type == pkt.IP_TYPE:
+				
+					#self.ip_to_ether[????] = pkt.src
+				
+					if payload.dstip in self.my_interfaces: #Sent to us, just dropping it for now
+						continue
+					
+					match = self.matchPrefix(payload.dstip)
+					
+					if match == None: #No entry on table matched
+						continue #drop the packet
+						
+					next_hop = match[1]
+					name = match[2]
+					
+					if next_hop == None: #No next hop, directly connected to our port
+						dst_ip = payload.dstip
+					else:
+						dst_ip = next_hop
+			
+					src_eth = self.nameMap[name][0] #Pull ip/eth corresponding to this interface
+					src_ip = self.nameMap[name][1]
+
+					payload.ttl -= 1
+					
+					ether = ethernet() #wrap ip in ether
+					ether.type = ether.IP_TYPE
+					ether.set_payload(payload)
+					ether.src = src_eth
+					
+					if dst_ip in self.ip_to_ether: #We have the mapping dst_ip->eth
+						ether.dst = self.ip_to_ether[dst_ip]
+						self.net.send_packet(name, ether) #Send packet on its way
+					else:					
+						if dst_ip in arp_ip: #Already waiting on ARP for this
+							arp_ip[dst_ip].addPacket(ether)
+						else: #New IP to ARP at
+							request	 = self.makeRequest(dst_ip, src_ip, src_eth) #create ARP req
+							waiter = arpWaiter(name, request, ether)
+							arp_ip[dst_ip] = waiter
 							
-		    if hwdst != None: #if we have that ip
-                        reply = self.makeReply(dst_ip, src_ip, hwdst, src_eth)
-                        
-                        self.net.send_packet(dev, reply) #send it off
-                elif pkt.type == pkt.IP_TYPE:
-                    forMe = False
-                    if payload.protodst in self.myIP:
-                        continue
+							self.net.send_packet(name, request) #Send ARP request
+						
+						
 
-                    payload.ttl -= 1
-                    match = self.matchPrefix(payload.dstip)
-                    
-                    src_eth = interface_by_name(match[2]).ethaddr
-                    src_ip = interface_by_name(match[2]).ipaddr
-                            
-                    print match
-                    if match[1] == None:
-                        dst_ip = payload.dstip
-                        print dst_ip, src_ip, src_eth
-                        request = self.makeRequest(dst_ip, src_ip, src_eth)
-                        self.net.send_packet(match[2], request) #not know next hop
-                    elif match[1] not in self.addToEth:
-                        request = self.makeRequest(match[1], src_ip, src_eth) # know next hop
-                        self.net.send_packet(match[2], request)
-                    else:
-                        ether = ethernet() #wrap in ether
-	                ether.type = ether.IP_TYPE
-	                ether.set_payload(payload)
-	                ether.src = src_eth
-                        ether.dst = self.addToEth[match[1]]
-
-                        self.net.send_packet(match[2], ether)
-                        
-                    
-	    except SrpyNoPackets:
-		# log_debug("Timeout waiting for packets")
-		continue
-	    except SrpyShutdown:
-		return
-
+			except SrpyNoPackets:
+				# log_debug("Timeout waiting for packets")
+				continue
+			except SrpyShutdown:
+				return
+				
+class arpWaiter(object):
+	def __init__(self, intf_name, arp_request, ether_pkt):
+		self.start_time = time.time()
+		self.tries = 1
+		
+		self.intf_name = intf_name
+		self.arp_req = arp_request
+		
+		self.packet_list = [ether_pkt]
+		
+	def addPacket(self, ether_pkt):
+		self.packet_list.append(ether_pkt)
+	
+	def getList(self):
+		return self.packet_list
 
 def srpy_main(net):
 	'''
