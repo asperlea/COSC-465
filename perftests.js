2da2b566c1a35c53a08280f90b76f5ede7f8e78f
Curt Mahoney <cwmahoney@colgate.edu>
2014-04-30 18:00:05 -0400

    Starting over from scratch _again_

--- perftests.js
+++
@@ -1,6 +0,0 @@
-exports.ping = function(req) {
-    // req is https://github.com/techpines/express.io/tree/master/lib#socketrequest
-    console.log("Got ping socket.io callback");
-};
-
-
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-sudo apt-get install npm
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-
